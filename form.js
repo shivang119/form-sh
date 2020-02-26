5409602b06f1b81bd03b35eb503b6a93efd7d9aa
shivang <shivang11goyal@gmail.com>
2020-02-26 11:24:56 +0530

    form trial

---
+++ form.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://shivang:shivang@cluster0-4mdfp.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri,{useNewUrlParser: true });
+
+
+app.use(express.urlencoded({
+  extended : true
+}))
+app.use(express.json())
+
+
+let f
+MongoClient.connect(uri, (err, client) => {
+  if (err) throw err;
+
+  taskdb = client.db("taskdb")
+  f = taskdb.collection("details")
+});
+  
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+'/fhtml.htm')
+});
+  app.post("/form",(req,res)=>{
+    console.log(req.body);
+    f.insertOne(req.body, (err, data) => {
+      if(err){
+        console.log(err)
+      }
+      else
+      {
+        console.log(data)
+        res.send("done")
+      }
+    })
+  
+  });
+  
+app.listen(3000,()=>{
+  console.log('Server started!')
+});
